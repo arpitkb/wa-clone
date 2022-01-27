59c56329b38a4d9947ecc3909de5d00b627d8a5f
arpitkb <bansal_851937@student.nitw.ac.in>
2022-01-28 02:51:53 +0530

    removed react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
-import { Container } from "react-bootstrap";
 import Dashboard from "./components/Dashboard";
 import Layout from "./components/Layout";
 import Login from "./components/Login";
 import useLocalStorage from "./hooks/useLocalStorage";
-import { Route, Routes } from "react-router-dom";
 import { ContactsProvider } from "./context/ContactsProvider";
 import { ConvoProvider } from "./context/ConvoProvider";
 import { SocketProvider } from "./context/SocketProvider";
@@ -12,25 +10,17 @@ function App() {
   const [id, setId] = useLocalStorage("id");
   return (
     <Layout>
-      <Routes>
-        <Route
-          exact
-          path='/'
-          element={
-            id ? (
-              <SocketProvider id={id}>
-                <ContactsProvider>
-                  <ConvoProvider id={id}>
-                    <Dashboard id={id} />
-                  </ConvoProvider>
-                </ContactsProvider>
-              </SocketProvider>
-            ) : (
-              <Login onSubmit={setId} />
-            )
-          }
-        ></Route>
-      </Routes>
+      {id ? (
+        <SocketProvider id={id}>
+          <ContactsProvider>
+            <ConvoProvider id={id}>
+              <Dashboard id={id} />
+            </ConvoProvider>
+          </ContactsProvider>
+        </SocketProvider>
+      ) : (
+        <Login onSubmit={setId} />
+      )}
     </Layout>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,5 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./Flatly.min.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
