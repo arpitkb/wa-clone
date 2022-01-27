5fabe8081d3a66de41cac2fb849e2d01cd1ad3ba
arpitkb <bansal_851937@student.nitw.ac.in>
2022-01-28 02:21:52 +0530

    fixed errors

--- server/server.js
+++ server/server.js
@@ -32,10 +32,10 @@ io.on("connection", (socket) => {
 // serve static assets in production
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static(path.resolve(__dirname, "../client", "build")));
+  app.use(express.static(path.join(__dirname, "../client", "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
   });
 }
 
